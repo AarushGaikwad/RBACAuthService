e0d3216d5bc3bded927eb1c4d9d48d6bd68ff115
AarushGaikwad <Aarushgaikwad789@gmail.com>
2025-07-09 12:00:09 +0530

    initial commit

---
+++ src/main/java/com/example/rbacAwtApi/RbacAwtApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.rbacAwtApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacAwtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbacAwtApiApplication.class, args);
+	}
+
+}
